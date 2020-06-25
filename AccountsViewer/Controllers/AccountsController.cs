3b9753097f1400290da0fd8c74833f801eff1511
reasonpolice <43945721+reasonpolice@users.noreply.github.com>
2020-06-25 15:06:15 -0600

    Add files via upload

---
+++ AccountsViewer/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AccountsCodingTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AccountsViewer.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Retrieve a list of accounts from the codingtest API
+        /// </summary>
+        /// <returns>A view displaying accounts sorted by status</returns>
+        public async Task<IActionResult> ListByStatus(int responsive = 0)
+        {
+            //I chose to call the API serverside because I would rather utilize razor page functionality than
+            //use an AJAX call and concatinate a bunch of html into a string in java
+            IEnumerable<Account> accountsList = new List<Account>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://frontiercodingtests.azurewebsites.net/api/accounts/getall"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    accountsList = JsonConvert.DeserializeObject<IEnumerable<Account>>(apiResponse);
+                }
+            }
+
+            if(responsive == 1)
+            {
+                return View("ListByStatusResponsive",accountsList.OrderBy(o => o.AccountStatusId).ToList());
+            }
+
+            return View(accountsList.OrderBy(o => o.AccountStatusId).ToList());
+        }
+
+
+    }
+}
---
+++ AccountsViewer/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AccountsViewer.Models;
+
+namespace AccountsViewer.Controllers
+{
+    //I added the backend project to this solution to account for future internal functionality
+    //Largely leaving this controller untouched
+    //alex r 6/25/2020
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
